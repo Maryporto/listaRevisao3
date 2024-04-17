baa1fffafc7743c888a9ada066c453b7e6f5ac01
Mariana Souza Porto <160690963+Maryporto@users.noreply.github.com>
2024-04-16 22:56:33 -0300

    Exercicio8.java

--- src/Exercicio8.java
+++ src/Exercicio8.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Exercicio8 {
+public class Exercicio8 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
 }
