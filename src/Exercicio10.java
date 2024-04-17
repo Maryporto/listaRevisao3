c101e8576c21d93af1ac755cc9b2d2a8d5b27b56
Mariana Souza Porto <160690963+Maryporto@users.noreply.github.com>
2024-04-16 22:55:47 -0300

    Exercicio10.java

--- src/Exercicio10.java
+++ src/Exercicio10.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Exercicio10 {
+public class Exercicio10 {
+    public static void main(String[] args) {
+        int soma = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                soma += i;
+                System.out.println(i);
+            }
+        }
+
+        System.out.println("Soma dos números pares de 1 a 100: " + soma);
+    }
 }
